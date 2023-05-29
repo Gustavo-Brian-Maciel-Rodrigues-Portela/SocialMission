f83736f7daa56c5a0ec390dd93ef960bdbe670ec
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2023-05-29 03:46:58 -0300

    Update campaing.js

--- Js/campaing.js
+++ Js/campaing.js
@@ -2,24 +2,24 @@ const telaTrending = document.getElementById("trendingContainer")
 
 function alterarTela1() {
     if (telaTrending.style.backgroundImage.indexOf('caco.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('burro.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/burro.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('burro.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('pingu.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/pingu.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('pingu.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('ratinho.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/ratinho.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('ratinho.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('caco.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/caco.jpg')";
     }
 }
 
 function alterarTela2() {
     if (telaTrending.style.backgroundImage.indexOf('caco.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('ratinho.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/ratinho.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('burro.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('caco.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/caco.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('pingu.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('burro.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/burro.jpg')";
     } else if (telaTrending.style.backgroundImage.indexOf('ratinho.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('pingu.jpg')";
+        telaTrending.style.backgroundImage = "url('Midia/imgCampaing/pingu.jpg')";
     }
 }
