c5fbc426cd93799f85e82e99a8aef2604f3cd95f
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2023-05-29 04:02:38 -0300

    Update campaing.js

--- Js/campaing.js
+++ Js/campaing.js
@@ -1,25 +1,33 @@
 const telaTrending = document.getElementById("trendingContainer");
 
 function alterarTela1() {
-  if (telaTrending.style.backgroundImage.includes('caco.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('burro.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('pingu.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('ratinho.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
+  if (telaTrending.classList.contains('imagem1')) {
+    telaTrending.classList.remove('imagem1');
+    telaTrending.classList.add('imagem2');
+  } else if (telaTrending.classList.contains('imagem2')) {
+    telaTrending.classList.remove('imagem2');
+    telaTrending.classList.add('imagem3');
+  } else if (telaTrending.classList.contains('imagem3')) {
+    telaTrending.classList.remove('imagem3');
+    telaTrending.classList.add('imagem4');
+  } else if (telaTrending.classList.contains('imagem4')) {
+    telaTrending.classList.remove('imagem4');
+    telaTrending.classList.add('imagem1');
   }
 }
 
 function alterarTela2() {
-  if (telaTrending.style.backgroundImage.includes('caco.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('burro.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('pingu.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
-  } else if (telaTrending.style.backgroundImage.includes('ratinho.jpg')) {
-    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
+  if (telaTrending.classList.contains('imagem1')) {
+    telaTrending.classList.remove('imagem1');
+    telaTrending.classList.add('imagem4');
+  } else if (telaTrending.classList.contains('imagem2')) {
+    telaTrending.classList.remove('imagem2');
+    telaTrending.classList.add('imagem1');
+  } else if (telaTrending.classList.contains('imagem3')) {
+    telaTrending.classList.remove('imagem3');
+    telaTrending.classList.add('imagem2');
+  } else if (telaTrending.classList.contains('imagem4')) {
+    telaTrending.classList.remove('imagem4');
+    telaTrending.classList.add('imagem3');
   }
 }
