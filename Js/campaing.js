7717cc57a05478aa02ec53a3554dfc7e855a5464
Gustavo-Brian-Maciel-Rodrigues-Portela <133496080+Gustavo-Brian-Maciel-Rodrigues-Portela@users.noreply.github.com>
2023-05-29 03:56:06 -0300

    Update campaing.js

--- Js/campaing.js
+++ Js/campaing.js
@@ -1,25 +1,25 @@
-const telaTrending = document.getElementById("trendingContainer")
+const telaTrending = document.getElementById("trendingContainer");
 
 function alterarTela1() {
-    if (telaTrending.style.backgroundImage.indexOf('caco.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('burro.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('pingu.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('ratinho.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
-    }
+  if (telaTrending.style.backgroundImage.includes('caco.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('burro.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('pingu.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('ratinho.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
+  }
 }
 
 function alterarTela2() {
-    if (telaTrending.style.backgroundImage.indexOf('caco.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('burro.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('pingu.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
-    } else if (telaTrending.style.backgroundImage.indexOf('ratinho.jpg') != -1) {
-        telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
-    }
+  if (telaTrending.style.backgroundImage.includes('caco.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/ratinho.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('burro.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/caco.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('pingu.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/burro.jpg')";
+  } else if (telaTrending.style.backgroundImage.includes('ratinho.jpg')) {
+    telaTrending.style.backgroundImage = "url('../Midia/imgCampaing/pingu.jpg')";
+  }
 }
